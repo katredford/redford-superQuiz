64e176c2587954fd385aab19085f1d9918bf5963
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-23 18:43:57 -0500

    add questions

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+        answer: "a"
+    },
+
+    {
+        prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+        answer: "a"
+    },
+
+    {
+        prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+        answer: "a"
+    },
+]
+
+var score = 0
