c99d86eb06acbc1a9a8c4e15f32f12197fba5c3f
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-23 18:56:35 -0500

    add for loop and if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,14 @@ var questions = [
 ]
 
 var score = 0
+
+for( var i=0; i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+        score==;
+        alert("correct!");
+    } else {
+        alert("WRONG!");
+    }
+}
+alert("you got " + score + "/" questions.lenght);
