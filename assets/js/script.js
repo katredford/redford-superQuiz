994b841b6d13145c994162e1aa9f899314b8ea0d
katredford <katharine.redford@gmail.com>
2021-03-26 19:05:35 -0500

    add note

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
-// var questions = [
-//     {
-//         prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
-//         answer: "a"
-//     },
+var questions = [
+    {
+        prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+        answer: [
+            {text: "4", correct: true },
+            {text: "22", correct: false }
+        ]
+    },
 
-//     {
-//         prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
-//         answer: "a"
-//     },
+    // {
+    //     prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+    //     answer: "a"
+    // },
 
-//     {
-//         prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
-//         answer: "a"
-//     },
-// ]
+    // {
+    //     prompt: "What 4 moons of jupiter did galileo discover? \n (a)farts \n (b)parts \n (c) darts",
+    //     answer: "a"
+    // },
+]
 
 // var score = 0
 
@@ -27,10 +30,25 @@
 //     }
 // 
 var startButton = document.getElementById("start-btn")
+var questionContainerElement = document.getElementById("question-container")
+var shuffledQuestions, currentQuestionIndex
+
+
 
 startButton.addEventListener("click",startGame)
 
 function startGame() {
 console.log('started')
+startButton.classList.add("hide")
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+questionContainerElement.classList.remove("hide")
+nextQuestion()
 }
 
+function nextQuestion() {
+     
+}
+
+function selectAnswer() {
+
+}
