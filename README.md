f506b285b6541bbb45d555fcdd685223defd4508
katredford <katharine.redford@gmail.com>
2021-07-19 10:29:54 -0500

    add screenshots, increase time limit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # redford-superQuiz
+
 <br>
-test your knowlege of random "facts"
+test your knowledge of random "facts"
 <br>
 <br><br>
 
@@ -8,11 +9,7 @@ test your knowlege of random "facts"
 
 can you answer all the questions correctly before the time runs out?
 
-
-
 ## Built With
-
-##### [Back to Table of Contents](#Table-of-Contents)
 
 - _HTML_
 - _CSS_
@@ -29,24 +26,11 @@ https://github.com/katredford/redford-superQuiz
 ## Screenshots
 
 <h3 align="center" width="100%">
- <br>1. Create New User</br>
+ <br>1. Start Quiz</br>
 <p align="center" width="100%">
-    <img src="./public/assets/images/new_user.png" width="60%"/>
+    <img src="./public/assets/images/start.png" width="60%"/>
 </p>
-2. Welcome, visit the cafe!
+2. Can you get all the questions right?
 <p align="center" width="100%">
-    <img src="./public/assets/images/homepage.png" width="60%"/>
+    <img src="./public/assets/images/quiz.png" width="60%"/>
 </p>
-3. Heres the menu, don't forget to be insulted
-<p align="center" width="100%">
-    <img src="./public/assets/images/menu.png" width="60%"/>
-</p>
-4. type a caption and a photo and brew that drink!
-<p align="center" width="100%">
-    <img src="./public/assets/images/post_roast.png" width="60%"/>
-</p>
-5. look what everyone posted!
-<p align="center" width="100%">
-    <img src="./public/assets/images/coffee_posts.png" width="60%"/>
-</p>
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,137 +1,127 @@
-
-
 // var time = questions.length * 15;
-var questionIndex = 0
-var gotRight = 1
-var counter = 5
+var questionIndex = 0;
+var gotRight = 1;
+var counter = 30;
 var questions = [
+	{
+		title:
+			"how long does it take for a photon in the center of the sun to reach the surface?",
+		correct: "one million years",
+		answers: [
+			"one million years",
+			"6 years",
+			"one billion years",
+			"one trillion years",
+		],
+	},
 
-         {title: 'how long does it take for a photon in the center of the sun to reach the surface?', 
-             correct : 'one million years',
-             answers : ["one million years", "6 years", "one billion years", "one trillion years"]
-            },
+	{
+		title: "which isnt a primary color?",
+		correct: "green",
+		answers: ["red", "green", "blue", "yellow"],
+	},
 
-        {title: 'which isnt a primary color?', 
-            correct : 'green',
-            answers : ["red", "green", "blue", "yellow"]
-            },
+	{
+		title: "which criptid is the best?",
+		correct: "nessie",
+		answers: ["mothman", "bigfoot", "hodag", "nessie"],
+	},
 
-        {title: 'which criptid is the best?', 
-            correct : 'nessie',
-            answers :["mothman", "bigfoot", "hodag", "nessie"]
-            },
+	{
+		title: "what to monarch caterpillars eat?",
+		correct: "milkweed",
+		answers: ["sandwich", "parsley", "milkweed", "daffodils"],
+	},
 
-        {title: 'what to monarch caterpillars eat?', 
-            correct : 'milkweed',
-            answers : ["sandwich", "parsley", "milkweed", "daffodils"]
-            },
-
-        {title: 'who stole fire from the gods?',
-            correct : 'prometheus',
-            answers : ["medusa", "prometheus", "hercules", "perseus"]
-            }
-
+	{
+		title: "who stole fire from the gods?",
+		correct: "prometheus",
+		answers: ["medusa", "prometheus", "hercules", "perseus"],
+	},
 ];
 
-function startQuiz(){
-    var startButton = document.getElementById("startpart")
-    startButton.addEventListener("click", startQuiz);
-    // document.getElementById("startpart").onclick = function() {
-    // // document.getElementById("startpart").style.display = "none";
-    //     return false;
-    //     } 
+function startQuiz() {
+	var startButton = document.getElementById("startpart");
+	startButton.addEventListener("click", startQuiz);
+	// document.getElementById("startpart").onclick = function() {
+	// // document.getElementById("startpart").style.display = "none";
+	//     return false;
+	//     }
 
-    document.getElementById("startpart").onclick = function(){
-        document.getElementById("startpart").style.display = "none";
-        document.getElementById("question-container").classList.remove("hide")
-        var countdown = function(){
-            console.log(counter);
-            counter--;
-            if(counter === 0){
-            clearInterval(startCountdown);
-          };
+	document.getElementById("startpart").onclick = function () {
+		document.getElementById("startpart").style.display = "none";
+		document.getElementById("question-container").classList.remove("hide");
+		var countdown = function () {
+			console.log(counter);
+			counter--;
+			if (counter === 0) {
+				clearInterval(startCountdown);
+			}
 
-          var timeBox = document.getElementById("timepart");
-          timeBox.innerHTML ="";
-          var timePlace = document.createElement("timeSpot")
-          timePlace.setAttribute("class","time")
-          timePlace.innerText = counter;
-      
-          timeBox.appendChild(timePlace)
+			var timeBox = document.getElementById("timepart");
+			timeBox.innerHTML = "";
+			var timePlace = document.createElement("timeSpot");
+			timePlace.setAttribute("class", "time");
+			timePlace.innerText = counter;
 
-        };
-        var startCountdown = setInterval(countdown,1000);
+			timeBox.appendChild(timePlace);
+		};
+		var startCountdown = setInterval(countdown, 1000);
+	};
 
-        
-        
-    };
-        
-    
-   
-   
+	function nextQuestion() {
+		var currentQuestion = questions[questionIndex];
+		var answerGrid = document.getElementById("answer-buttons");
+		answerGrid.innerHTML = "";
 
- function nextQuestion() {
-    var currentQuestion = questions[questionIndex];  
-    var answerGrid = document.getElementById("answer-buttons");
-    answerGrid.innerHTML = "";
+		var questionTitle = document.getElementById("question");
+		questionTitle.innerText = currentQuestion.title;
+		currentQuestion.answers.forEach(function (answer, i) {
+			//1 Make the piece of html in js
+			var anwserButton = document.createElement("button");
+			//2 Dress that html up how u want!! give it text . class name ect...
+			anwserButton.setAttribute("class", "btn choice-btn");
+			anwserButton.innerText = answer;
 
-    var questionTitle = document.getElementById("question")
-    questionTitle.innerText = currentQuestion.title
-    currentQuestion.answers.forEach(function(answer,i){
+			//3 Stick that shabang on the page!! .appendCHild or jquery .append()
+			answerGrid.appendChild(anwserButton);
+		});
 
-        //1 Make the piece of html in js
-        var anwserButton = document.createElement("button");
-        //2 Dress that html up how u want!! give it text . class name ect...
-        anwserButton.setAttribute("class","btn choice-btn")
-        anwserButton.innerText = answer
-        
-        //3 Stick that shabang on the page!! .appendCHild or jquery .append()
-        answerGrid.appendChild(anwserButton)
-    })
-    
-    var buttons = document.querySelectorAll(".choice-btn")
-    console.log('buttons ?', buttons)
-   
-    buttons.forEach(function(btn)  {
-        btn.addEventListener("click", handleClick);
-    })
+		var buttons = document.querySelectorAll(".choice-btn");
+		console.log("buttons ?", buttons);
 
- } 
- nextQuestion()
+		buttons.forEach(function (btn) {
+			btn.addEventListener("click", handleClick);
+		});
+	}
+	nextQuestion();
 
+	function handleClick(event) {
+		console.log("we got clicked", event.target.innerText);
 
+		if (event.target.innerText === questions[questionIndex].correct) {
+			console.log("good", gotRight);
 
- function handleClick (event) {
-    console.log('we got clicked', event.target.innerText)
-    
+			var scoreBox = document.getElementById("scorepart");
+			scoreBox.innerHTML = "";
+			var scorePlace = document.createElement("scoreSpot");
+			scorePlace.setAttribute("class", "score");
+			scorePlace.innerText = gotRight++;
 
-   if (event.target.innerText === questions[questionIndex].correct) {
-    console.log('good',gotRight) 
+			scoreBox.appendChild(scorePlace);
+		} else {
+			console.log("bad");
+		}
 
-    var scoreBox = document.getElementById("scorepart");
-    scoreBox.innerHTML ="";
-    var scorePlace = document.createElement("scoreSpot")
-    scorePlace.setAttribute("class","score")
-    scorePlace.innerText = gotRight++
+		questionIndex++;
 
-    scoreBox.appendChild(scorePlace)
-
-   } else {
-       console.log('bad')
-
-    }
-
-    questionIndex++
-
-    if (questions.length <= questionIndex) {
-        document.getElementById("scoreinput").classList.remove("hide")
-        document.getElementById("question-container").classList.add("hide")
-        console.log("gameOVER")
-    } else {
-        nextQuestion()
-    }
-    
-
-}; 
-
-}; startQuiz();
+		if (questions.length <= questionIndex) {
+			document.getElementById("scoreinput").classList.remove("hide");
+			document.getElementById("question-container").classList.add("hide");
+			console.log("gameOVER");
+		} else {
+			nextQuestion();
+		}
+	}
+}
+startQuiz();
